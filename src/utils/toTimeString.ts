a8e6eb2b5f05139dd6e794a155713e6aca6e46da
Son Thanh Vo <sonvo95@outlook.com>
2021-11-21 17:39:50 +0100

    feat: add leading zeroes to time string

--- src/utils/toTimeString.ts
+++ src/utils/toTimeString.ts
@@ -1,6 +1,14 @@
+function pad(value: number, length: number): string {
+  return value.toString().padStart(length, '000');
+}
+
 export function toTimeString(start: number, finish: number): string {
   const elapsed = finish - start;
   const time = new Date(elapsed);
 
-  return `${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
+  const minutes = pad(time.getMinutes(), 2);
+  const seconds = pad(time.getSeconds(), 2);
+  const milliseconds = pad(time.getMilliseconds(), 3);
+
+  return `${minutes}:${seconds}:${milliseconds}`;
 }
