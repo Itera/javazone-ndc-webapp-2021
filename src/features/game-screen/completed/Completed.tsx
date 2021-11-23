03a0a9580ea776d8fdb9a18ca83abd32aa857171
Son Thanh Vo <sonvo95@outlook.com>
2021-11-23 17:44:52 +0100

    feat: add time display after finish

--- src/features/game-screen/completed/Completed.tsx
+++ src/features/game-screen/completed/Completed.tsx
@@ -1,10 +1,46 @@
+import { motion } from "framer-motion";
 import { useLocation } from "react-router";
 import { toTimeString } from "../../../utils/toTimeString";
+import { Highscore } from "../../firebase/Highscore";
 
 export function Completed() {
   const location = useLocation();
   const run = location.state;
   const elapsed = toTimeString(run.start, run.finish);
+  const [minutes, seconds, milliseconds] = elapsed.split(":");
 
-  return <p>GG {elapsed}</p>;
+  return (
+    <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
+      <motion.section
+        style={{
+          backgroundColor: "#EEEDE4",
+          textAlign: "center",
+          color: "#0029FF",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexGrow: 10,
+        }}
+        animate={{ flexGrow: 1 }}
+        transition={{ duration: 2, delay: 3 }}
+      >
+        <p
+          style={{
+            fontWeight: "bold",
+          }}
+        >
+          <span style={{ fontSize: 160 }}>{minutes}</span>
+          <span style={{ fontSize: 160 }}>:{seconds}</span>
+          <span style={{ fontSize: 96 }}>:{milliseconds}</span>
+        </p>
+      </motion.section>
+      <motion.section
+        style={{ flexGrow: 0, height: 0 }}
+        animate={{ flexGrow: 100, height: "auto" }}
+        transition={{ duration: 2, delay: 3 }}
+      >
+        <Highscore />
+      </motion.section>
+    </div>
+  );
 }
