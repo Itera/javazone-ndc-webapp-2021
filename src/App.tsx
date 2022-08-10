10d0351d36c7f49592644a7b0ea01ef4770a04b5
Son Thanh Vo <sonvo95@outlook.com>
2022-08-10 19:15:54 +0200

    refactor: remove google analytics page tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { RouteListener } from './routes/RouteListener';
 import { Router } from './routes';
 
 function App() {
   return (
     <main>
-      <RouteListener />
       <Router />
     </main>
   );
