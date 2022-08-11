947d504f032fb22c2f7df645883d49815dd9ed66
Son Thanh Vo <sonvo95@outlook.com>
2022-08-11 18:55:37 +0200

    chore: remove commented out code

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,22 +1,10 @@
-import { DatabaseReference, child, update, remove } from "@firebase/database";
-import {
-  User,
-  //UserCredential,
-  //signInAnonymously,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { DatabaseReference, child, remove, update } from '@firebase/database';
+import { User, signInWithEmailAndPassword } from 'firebase/auth';
 
-//import { Auth } from 'firebase/auth';
-import { useDatabase } from "./useDatabase";
-import { useFirebase } from "../features/firebase/FirebaseProvider";
-import { Logger } from "../features/logging/logger";
-import { useState } from "react";
-
-/* async function createUser(auth: Auth, logger: Logger): Promise<UserCredential> {
-  logger.trace('Creating a new user');
-  const credentials = await signInAnonymously(auth);
-  return credentials;
-} */
+import { Logger } from '../features/logging/logger';
+import { useDatabase } from './useDatabase';
+import { useFirebase } from '../features/firebase/FirebaseProvider';
+import { useState } from 'react';
 
 async function addUserToCollection(
   db: DatabaseReference,
@@ -25,9 +13,9 @@ async function addUserToCollection(
   phone: string,
   consent: boolean,
   username: string,
-  logger: Logger
+  logger: Logger,
 ): Promise<void> {
-  logger.trace("Creating a document entry for user", `[uid=${phone}]`);
+  logger.trace('Creating a document entry for user', `[uid=${phone}]`);
   const documentRef = child(db, `/${phone}`);
   await update(documentRef, {
     email,
@@ -42,15 +30,15 @@ async function addUserToCollection(
 async function removeFromUnregistered(
   db: DatabaseReference,
   entry: string,
-  logger: Logger
+  logger: Logger,
 ): Promise<void> {
-  logger.trace("Removing from Unregistered", `[entry=${entry}]`);
+  logger.trace('Removing from Unregistered', `[entry=${entry}]`);
   const documentRef = child(db, `/${entry}`);
   await remove(documentRef);
 }
 
 export function useAuth() {
-  const [logger] = useState(new Logger("useAuth"));
+  const [logger] = useState(new Logger('useAuth'));
   const { users, unregistered } = useDatabase();
   const { auth } = useFirebase();
 
@@ -60,9 +48,9 @@ export function useAuth() {
     email: string,
     consent: boolean,
     entry: string,
-    username: string
+    username: string,
   ): Promise<null | User> {
-    logger.trace("Adding new user");
+    logger.trace('Adding new user');
     await addUserToCollection(
       users,
       email,
@@ -70,25 +58,25 @@ export function useAuth() {
       phone,
       consent,
       username,
-      logger
+      logger,
     );
 
-    logger.info("Successfully created new user", `[uid=${phone}]`);
+    logger.info('Successfully created new user', `[uid=${phone}]`);
 
     await removeFromUnregistered(unregistered, entry, logger);
     logger.info(
-      "Successfully removed user from unregistered",
-      `[uid=${entry}]`
+      'Successfully removed user from unregistered',
+      `[uid=${entry}]`,
     );
 
     return null;
   }
 
   async function signIn(email: string, password: string): Promise<User> {
-    logger.trace("Signing in user");
+    logger.trace('Signing in user');
     const credentials = await signInWithEmailAndPassword(auth, email, password);
 
-    logger.info("Signed in user", `[uid=${credentials.user.uid}]`);
+    logger.info('Signed in user', `[uid=${credentials.user.uid}]`);
     return credentials.user;
   }
 
