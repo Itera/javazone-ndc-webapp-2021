f9a3830c37ca553444e10f833ffbc345a73ddc11
Son Thanh Vo <sonvo95@outlook.com>
2022-09-05 14:25:36 +0200

    feat: add back button

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,17 +1,18 @@
-import { ReactComponent } from "../statics/svgs/Arrow.svg";
+import { ReactComponent } from '../statics/svgs/Arrow.svg';
 
 interface Props {
   readonly color?: string;
+  readonly width?: string;
+  readonly style?: Record<string, any>;
 }
 
 export function Arrow(props: Props) {
-  const { color = "#fff" } = props;
+  const {
+    color = '#fff',
+    width = '80px',
+    style = { marginRight: '2rem' },
+  } = props;
   return (
-    <ReactComponent
-      stroke={color}
-      fill={color}
-      width="80px"
-      style={{ marginRight: "2rem" }}
-    />
+    <ReactComponent stroke={color} fill={color} width={width} style={style} />
   );
 }
