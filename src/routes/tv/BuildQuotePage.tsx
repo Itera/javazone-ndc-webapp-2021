dc56b2588116f00161b7a9724b528f42d090d9fa
Son Thanh Vo <sonvo95@outlook.com>
2022-10-02 12:29:24 +0200

    feat: redirect on invalid navigation

--- src/routes/tv/BuildQuotePage.tsx
+++ src/routes/tv/BuildQuotePage.tsx
@@ -1,4 +1,26 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { Logger } from '../../service/logger';
+import { Paths } from '../Router';
+import { useMount } from '../../hooks/useMount';
+
+const logger = new Logger('BuildQuotePage');
+
+function useViewModel() {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  useMount(() => {
+    if (state === null || state.username === undefined) {
+      logger.warn('Attempted to access build page without username in state');
+      navigate(Paths.SIGN_UP);
+    }
+  });
+}
+
 function BuildQuote(): JSX.Element {
+  useViewModel();
+
   return <h1>Hello</h1>;
 }
 
