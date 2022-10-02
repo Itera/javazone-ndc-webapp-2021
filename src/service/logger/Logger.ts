8fc9806335f23eb1247fd60555ae71d0e2e5d7bb
Son Thanh Vo <sonvo95@outlook.com>
2022-10-02 10:36:01 +0200

    feat: add log level filter

--- src/service/logger/Logger.ts
+++ src/service/logger/Logger.ts
@@ -1,3 +1,20 @@
+type Level = 'trace' | 'debug' | 'info' | 'warn' | 'error';
+const hierachy: Array<Level> = ['trace', 'debug', 'info', 'warn', 'error'];
+
+const defaultLogLevel =
+  process.env.NODE_ENV === 'development' ? 'trace' : 'warn';
+const logLevel = process.env.REACT_APP_LOG_LEVEL ?? defaultLogLevel;
+const logPriority = hierachy.indexOf(logLevel as Level);
+
+function enabledLogLevel(level: string): boolean {
+  const priority = hierachy.indexOf(level as Level);
+
+  if (priority === -1) {
+    throw new Error(`level must be one of [${hierachy}]`);
+  }
+
+  return priority >= logPriority;
+}
 export class Logger {
   private name;
 
@@ -5,23 +22,29 @@ export class Logger {
     this.name = name;
   }
 
+  private logMessage(level: Level, ...params: Array<unknown>): void {
+    if (enabledLogLevel(level)) {
+      console[level](`[name=${this.name}]`, ...params);
+    }
+  }
+
   trace(...params: Array<unknown>): void {
-    console.trace(`[name=${this.name}]`, ...params);
+    this.logMessage('trace', ...params);
   }
 
   debug(...params: Array<unknown>): void {
-    console.debug(`[name=${this.name}]`, ...params);
+    this.logMessage('debug', ...params);
   }
 
   info(...params: Array<unknown>): void {
-    console.info(`[name=${this.name}]`, ...params);
+    this.logMessage('info', ...params);
   }
 
   warn(...params: Array<unknown>): void {
-    console.warn(`[name=${this.name}]`, ...params);
+    this.logMessage('warn', ...params);
   }
 
   error(...params: Array<unknown>): void {
-    console.error(`[name=${this.name}]`, ...params);
+    this.logMessage('error', ...params);
   }
 }
